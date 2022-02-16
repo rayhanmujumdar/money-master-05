8165af836caf91daffbc3e557628c90ee29cd24e
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-02-17 01:58:39 +0600

    added input id names

---
+++ js/apps.js
@@ -0,0 +1,11 @@
+function htmlId(id){
+    return document.getElementById(id)
+}
+function incomeOrExpenses(){
+    
+}
+const expensesCalButton = htmlId("expenses-cal-button")
+expensesCalButton.addEventListener("click",()=>{
+    const income = htmlId("income-value");
+    const incomeValue = income.value;
+})
