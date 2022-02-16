ea9eb37e5a980e0f565116dbca3d3af9763ba458
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-02-17 03:05:56 +0600

    error condition added

--- js/apps.js
+++ js/apps.js
@@ -15,13 +15,35 @@ function costCalculation(){
     const foodCost = costInputValue("food-cost-input")
     const rentCost = costInputValue("rent-cost-input");
     const ClothesCost = costInputValue("clothes-cost-input");
-    // Total expenses
-    const totalCost = foodCost + rentCost + ClothesCost;
+    // html elements id;
+    const errorMsg = htmlId("error-msg");
     const totalExpenses = htmlId("total-cost");
-    totalExpenses.innerHTML = totalCost;
+    const balanceId = htmlId("total-balance");
+    // error condition;
+    if(isNaN(incomeValue) || isNaN(foodCost) || isNaN(rentCost) || isNaN(ClothesCost)){
+        errorMsg.style.display = "block"
+        errorMsg.innerText = "This is a wrong Value!"
+    }else{
+        // Total expenses
+        const totalCost = foodCost + rentCost + ClothesCost;
+        if(totalCost < incomeValue){
+            // const totalCost = foodCost + rentCost + ClothesCost;
+            totalExpenses.innerHTML = totalCost;
+            const totalBalance = incomeValue - totalCost;
+            balanceId.innerText = totalBalance;
+            errorMsg.style.display = "none"
+        }else{
+            errorMsg.style.display = "block"
+            errorMsg.innerText = "Expenses outweigh your income"
+            htmlId("income-value").value = "";
+            htmlId("food-cost-input").value = "";
+            htmlId("rent-cost-input").value = "";
+            htmlId("clothes-cost-input").value = "";
+        }
+    }
 }
 // calculate button click;
 const expensesCalButton = htmlId("expenses-cal-button")
 expensesCalButton.addEventListener("click",()=>{
-    console.log(costCalculation())
+    costCalculation()
 })
