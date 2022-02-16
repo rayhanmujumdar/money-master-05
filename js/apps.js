e513366f51503f1847a385aac3d52c2baecd7625
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-02-17 04:08:17 +0600

    saving amount button added

--- js/apps.js
+++ js/apps.js
@@ -45,8 +45,30 @@ function empty(){
         htmlId(empty).value = ""
     }
 }
-// calculate button click;
+// calculate button click event;
 const expensesCalButton = htmlId("expenses-cal-button")
 expensesCalButton.addEventListener("click",()=>{
     costCalculation()
 });
+
+// savinvg button click event;
+const savingButton = htmlId("saving-button");
+savingButton.addEventListener("click",()=>{
+    // saving amount;
+    const incomeValue = costInputValue("income-value");
+    const PercentageValue = costInputValue("Percentage-input");
+    const savingAmount = incomeValue * (PercentageValue / 100);
+    const yourSaving = htmlId("your-saving-amount");
+    yourSaving.innerText = savingAmount;
+    const totalBalance = htmlId("total-balance").innerText;
+    if(savingAmount > totalBalance){
+        const errorMsgSaving = htmlId("error-msg-saving");
+        errorMsgSaving.style.display = 'block'
+    }else{
+        // remaining balance;
+        const totalRemainingBalance = totalBalance - savingAmount;
+        const remainingBalance = htmlId("remaining-Balance");
+        remainingBalance.innerHTML = totalRemainingBalance;
+    }
+    // console.log(savingAmount)
+})
