57388d3ed03bafebe2fb3541332043c09b079431
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-02-17 02:23:59 +0600

    expenses total cost output added

--- js/apps.js
+++ js/apps.js
@@ -1,11 +1,27 @@
+// all id selector function
 function htmlId(id){
     return document.getElementById(id)
 }
-function incomeOrExpenses(){
-    
+// cost input value;
+function costInputValue(id){
+    const costId = htmlId(id);
+    const costValue = parseFloat(costId.value);
+    return costValue;
 }
+// cost calculation
+function costCalculation(){
+    // input value;
+    const incomeValue = costInputValue("income-value");
+    const foodCost = costInputValue("food-cost-input")
+    const rentCost = costInputValue("rent-cost-input");
+    const ClothesCost = costInputValue("clothes-cost-input");
+    // Total expenses
+    const totalCost = foodCost + rentCost + ClothesCost;
+    const totalExpenses = htmlId("total-cost");
+    totalExpenses.innerHTML = totalCost;
+}
+// calculate button click;
 const expensesCalButton = htmlId("expenses-cal-button")
 expensesCalButton.addEventListener("click",()=>{
-    const income = htmlId("income-value");
-    const incomeValue = income.value;
+    console.log(costCalculation())
 })
